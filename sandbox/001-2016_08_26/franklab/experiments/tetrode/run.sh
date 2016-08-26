6f7149a9e52a0d0f2cb4e2a8dec6850407a380e7
Jeremy Magland <jeremy.magland@gmail.com>
2016-08-26 14:21:45 -0400

    reorganize

--- sandbox/001-2016_08_26/franklab/experiments/tetrode/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-raw=../../raw/FL_20160426_r1_nt16
-
-mkdir output
-mountainprocess run-script --_nodaemon alg_scda_009.js $raw/params.json --raw=$raw/raw.mda.prv --outpath=output | tee sort.log
