75bbf6693b5c423f5a1c68ae89c0958e5f7e8837
Jeremy Magland <jeremy.magland@gmail.com>
2016-09-20 15:43:24 -0400

    add

---
+++ sandbox/005-2016-09-20/EJ/jfm_parse.m
@@ -0,0 +1,64 @@
+function jfm_parse
+
+mfile_path=fileparts(mfilename('fullpath'))
+
+raw_path = '/disk1/prvdata/EJ/'
+
+jar_file = strcat(raw_path, 'Vision/Vision.jar')
+javaaddpath(jar_file)
+
+add_path = '2005-04-26-0/data002/'
+full_path = strcat(raw_path, add_path)
+rawFile = edu.ucsc.neurobiology.vision.io.RawDataFile(full_path)
+
+% rawFile = edu.ucsc.neurobiology.vision.io.RawDataFile( \
+%   '/usr/local/google/home/bhaishahster/Downloads/data002/');
+
+% One can check dataset details (duration, number of electrodes, etc):
+% the following :
+% rawFile.getHeader()
+% the geometry of the electrodes is in 'Electrode positions' folder.
+
+% gets data from sample 0 to 20000 (first second) across all electrodes
+time_len  = 60*5; % in seconds
+samplingRate = 20000;
+channels=[131,132,138,139,140,147,148];
+
+raw=zeros(length(channels),samplingRate*time_len);
+for s=1:time_len
+    disp(s);
+    data_len = samplingRate * 1; % 1 = one second
+    data = rawFile.getData(data_len*(s-1)+1, data_len); % start time, length of datasets
+    disp(size(data));
+    raw(:,data_len*(s-1)+1:data_len*s)=data(:,channels)';
+end
+
+rawFile.close();
+
+outpath=[raw_path,'/raw_mda/2005-04-26-0/data002'];
+outpath2=[mfile_path,'/datasets/2005-04-26-0/data002'];
+mkdir(outpath);
+mkdir(outpath2);
+writemda(raw,[outpath,'/raw.mda'],'uint16');
+system(sprintf('prv-create %s/raw.mda %s/raw.mda.prv',outpath,outpath2));
+write_text_file([outpath2,'/params.json'],'{"samplerate":20000}');
+
+geom=csvread('512coords.txt');
+geom=geom(channels,:);
+csvwrite([outpath2,'/geom.csv'],geom);
+
+
+% Stimulus TTLs (most times every 100 frames) on java electrode
+% position 0 (1 in matlab) other electrodes are raw voltage traces
+%figure;
+%plot(data(:,1:3));
+
+end
+
+function write_text_file(fname,txt)
+
+fid = fopen(fname,'wt');
+fprintf(fid, '%s',txt);
+fclose(fid);
+
+end
