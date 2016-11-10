aa711056d140e32e59a99b63d19e4ba4a0ce6c3b
Jeremy Magland <jeremy.magland@gmail.com>
2016-11-10 18:36:57 -0500

    speed tests

--- sandbox/005-2016-09-20/neuron_paper_7hr_probe/run_speed_tests.sh
+++ sandbox/005-2016-09-20/neuron_paper_7hr_probe/run_speed_tests.sh
@@ -1,16 +1,16 @@
-kron-run ms2_30min np7 --_force_run > out_ms2_30min.txt
-kron-run ms2_60min np7 --_force_run > out_ms2_60min.txt
-kron-run ms2_120min np7 --_force_run > out_ms2_120min.txt
+#kron-run ms2_30min np7 --_force_run > out_ms2_30min.txt
+#kron-run ms2_60min np7 --_force_run > out_ms2_60min.txt
+#kron-run ms2_120min np7 --_force_run > out_ms2_120min.txt
 kron-run ms2_240min np7 --_force_run > out_ms2_240min.txt
-kron-run ms2 np7 --_force_run > out_ms2.txt
+#kron-run ms2 np7 --_force_run > out_ms2.txt
 
-kron-run ms2_120min_ch4 np7 --_force_run > out_ms2_120min_ch4.txt
-kron-run ms2_120min_ch8 np7 --_force_run > out_ms2_120min_ch8.txt
-kron-run ms2_120min_ch12 np7 --_force_run > out_ms2_120min_ch12.txt
+#kron-run ms2_120min_ch4 np7 --_force_run > out_ms2_120min_ch4.txt
+#kron-run ms2_120min_ch8 np7 --_force_run > out_ms2_120min_ch8.txt
+#kron-run ms2_120min_ch12 np7 --_force_run > out_ms2_120min_ch12.txt
 
-kron-run ms2_120min_thr32 np7 --_force_run > out_ms2_120min_thr32.txt
-kron-run ms2_120min_thr16 np7 --_force_run > out_ms2_120min_thr16.txt
-kron-run ms2_120min_thr8 np7 --_force_run > out_ms2_120min_thr8.txt
-kron-run ms2_120min_thr4 np7 --_force_run > out_ms2_120min_thr4.txt
-kron-run ms2_120min_thr2 np7 --_force_run > out_ms2_120min_thr2.txt
+#kron-run ms2_120min_thr32 np7 --_force_run > out_ms2_120min_thr32.txt
+#kron-run ms2_120min_thr16 np7 --_force_run > out_ms2_120min_thr16.txt
+#kron-run ms2_120min_thr8 np7 --_force_run > out_ms2_120min_thr8.txt
+#kron-run ms2_120min_thr4 np7 --_force_run > out_ms2_120min_thr4.txt
+#kron-run ms2_120min_thr2 np7 --_force_run > out_ms2_120min_thr2.txt
 
