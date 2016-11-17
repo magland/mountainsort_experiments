698f1d4e9c84d653068f6c1ec565e95262f645c3
Jeremy Magland <jeremy.magland@gmail.com>
2016-11-16 19:10:55 -0500

    dev

--- sandbox/005-2016-09-20/neuron_paper_7hr_probe/run_speed_tests.sh
+++ sandbox/005-2016-09-20/neuron_paper_7hr_probe/run_speed_tests.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 echo "10min $(date)"
-kron-run ms2_10min np7 --_force_run > out_ms2_10min.txt
+#kron-run ms2_10min np7 --_force_run > out_ms2_10min.txt
 echo "30min $(date)"
-kron-run ms2_30min np7 --_force_run > out_ms2_30min.txt
+#kron-run ms2_30min np7 --_force_run > out_ms2_30min.txt
 echo "60min $(date)"
-kron-run ms2_60min np7 --_force_run > out_ms2_60min.txt
+#kron-run ms2_60min np7 --_force_run > out_ms2_60min.txt
 echo "120min $(date)"
-kron-run ms2_120min np7 --_force_run > out_ms2_120min.txt
+#kron-run ms2_120min np7 --_force_run > out_ms2_120min.txt
 echo "240min $(date)"
-kron-run ms2_240min np7 --_force_run > out_ms2_240min.txt
+#kron-run ms2_240min np7 --_force_run > out_ms2_240min.txt
 echo "420min $(date)"
-kron-run ms2 np7 --_force_run > out_ms2.txt
+#kron-run ms2 np7 --_force_run > out_ms2.txt
 
 echo "ch4 $(date)"
-kron-run ms2_120min_ch4 np7 --_force_run > out_ms2_120min_ch4.txt
+#kron-run ms2_120min_ch4 np7 --_force_run > out_ms2_120min_ch4.txt
 echo "ch8 $(date)"
-kron-run ms2_120min_ch8 np7 --_force_run > out_ms2_120min_ch8.txt
+#kron-run ms2_120min_ch8 np7 --_force_run > out_ms2_120min_ch8.txt
 echo "ch12 $(date)"
-kron-run ms2_120min_ch12 np7 --_force_run > out_ms2_120min_ch12.txt
+#kron-run ms2_120min_ch12 np7 --_force_run > out_ms2_120min_ch12.txt
 
-echo thr32 "$(date)$
+echo "thr32 $(date)"
 kron-run ms2_120min_thr32 np7 --_force_run > out_ms2_120min_thr32.txt
-echo thr16 "$(date)$
-kron-run ms2_120min_thr16 np7 --_force_run > out_ms2_120min_thr16.txt
-echo thr8 "$(date)$
+echo "thr16 $(date)"
+#kron-run ms2_120min_thr16 np7 --_force_run > out_ms2_120min_thr16.txt
+echo "thr8 $(date)"
 kron-run ms2_120min_thr8 np7 --_force_run > out_ms2_120min_thr8.txt
-echo thr4 "$(date)$
-kron-run ms2_120min_thr4 np7 --_force_run > out_ms2_120min_thr4.txt
-echo thr2 "$(date)$
+echo "thr4 $(date)"
+#kron-run ms2_120min_thr4 np7 --_force_run > out_ms2_120min_thr4.txt
+echo "thr2 $(date)"
 kron-run ms2_120min_thr2 np7 --_force_run > out_ms2_120min_thr2.txt
 
