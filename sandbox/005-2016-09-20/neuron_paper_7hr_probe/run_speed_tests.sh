ae66a065bae1f0ab977bd1feb8fb06700b5cf196
Jeremy Magland <jeremy.magland@gmail.com>
2016-11-17 16:00:34 -0500

    writing paper

--- sandbox/005-2016-09-20/neuron_paper_7hr_probe/run_speed_tests.sh
+++ sandbox/005-2016-09-20/neuron_paper_7hr_probe/run_speed_tests.sh
@@ -16,18 +16,18 @@ echo "420min $(date)"
 echo "ch4 $(date)"
 #kron-run ms2_120min_ch4 np7 --_force_run > out_ms2_120min_ch4.txt
 echo "ch8 $(date)"
-#kron-run ms2_120min_ch8 np7 --_force_run > out_ms2_120min_ch8.txt
+kron-run ms2_120min_ch8 np7 --_force_run > out_ms2_120min_ch8.txt
 echo "ch12 $(date)"
 #kron-run ms2_120min_ch12 np7 --_force_run > out_ms2_120min_ch12.txt
 
-echo "thr32 $(date)"
-kron-run ms2_120min_thr32 np7 --_force_run > out_ms2_120min_thr32.txt
 echo "thr16 $(date)"
 #kron-run ms2_120min_thr16 np7 --_force_run > out_ms2_120min_thr16.txt
 echo "thr8 $(date)"
-kron-run ms2_120min_thr8 np7 --_force_run > out_ms2_120min_thr8.txt
+#kron-run ms2_120min_thr8 np7 --_force_run > out_ms2_120min_thr8.txt
 echo "thr4 $(date)"
 #kron-run ms2_120min_thr4 np7 --_force_run > out_ms2_120min_thr4.txt
 echo "thr2 $(date)"
-kron-run ms2_120min_thr2 np7 --_force_run > out_ms2_120min_thr2.txt
+#kron-run ms2_120min_thr2 np7 --_force_run > out_ms2_120min_thr2.txt
+echo "thr1 $(date)"
+#kron-run ms2_120min_thr1 np7 --_force_run > out_ms2_120min_thr1.txt
 
