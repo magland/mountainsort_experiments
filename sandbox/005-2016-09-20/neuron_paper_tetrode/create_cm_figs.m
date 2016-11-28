530892056dfe0578387b8c4711029acdbabb6167
Jeremy Magland <jeremy.magland@gmail.com>
2016-11-28 11:45:22 -0500

    tweaks

--- sandbox/005-2016-09-20/neuron_paper_tetrode/create_cm_figs.m
+++ sandbox/005-2016-09-20/neuron_paper_tetrode/create_cm_figs.m
@@ -6,7 +6,7 @@ fs2=20;
 conf_count_plot(csvread('manual1-vs-manual2.csv'),font_size,'');
 xlabel('Manual 2','FontSize',fs2);
 ylabel('Manual 1','FontSize',fs2);
-saveas(gcf,'manual1-vs-manual2.png');
+zsaveas(gcf,'manual1-vs-manual2.png');
 
 conf_count_plot(csvread('manual1-vs-manual3.csv'),font_size,'');
 xlabel('Manual 3','FontSize',fs2);
