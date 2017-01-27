a56b0a83f3064bee4bc0eb73ea177a040a655c97
Jeremy Magland <jeremy.magland@gmail.com>
2017-01-27 07:19:02 -0500

    explore drift

--- sandbox/torben_test1/extract_segment_of_clips.m
+++ sandbox/torben_test1/extract_segment_of_clips.m
@@ -1,3 +1,4 @@
-function clips=extract_segment_of_clips(clips,times,t1,t2)
+function [clips,features]=extract_segment_of_clips(clips,features,times,t1,t2)
 inds=find((t1<=times)&(times<=t2));
 clips=clips(:,:,inds);
+features=features(:,inds);
--- sandbox/torben_test1/test_handle_drift_exploration.m
+++ sandbox/torben_test1/test_handle_drift_exploration.m
@@ -1,64 +1,57 @@
+%fprintf('Loading...\n');
 %L=load('test_handle_drift.mat');
 clips=L.data.clips;
 labels=L.data.labels;
 times0=L.data.times;
 
-
 num_features=20;
-num_neighbors=10;
-segment_size=30000*60;
+segment_size=30000*60*10;
+segment_step=30000*60;
 
+fprintf('Computing features...\n');
+features=ms_event_features(clips,num_features);
+
+M=size(clips,1);
+T=size(clips,2);
 N=max(times0);
-t1s=1:segment_size:N;
+t1s=1:segment_step:N;
 if (length(t1s)>1)
     t1s=t1s(1:end-1);
 end;
 
+fprintf('Defining segments...\n');
 segments={};
+LL=length(times0);
+labels=zeros(1,LL);
 for j=1:length(t1s)
     S.t1=t1s(j);
     if (j<length(t1s))
-        S.t2=t1s(j+1)-1;
+        S.t2=min(N,t1s(j)+segment_size-1);
     else
         S.t2=N;
     end;
-    S.clips=extract_segment_of_clips(clips,times0,S.t1,S.t2);
+    S.inds=find((S.t1<=times0)&(times0<=S.t2));
     segments{j}=S;
 end;
 
-segments{1}.clips_traced=segments{1}.clips;
-for j=2:length(segments)
-    fprintf('Segment %d/%d\n',j,length(segments));
-    clips1=segments{j-1}.clips;
-    clips2=segments{j}.clips;
-    N1=size(clips1,3);
-    N2=size(clips2,3);
-    FF=ms_event_features(cat(3,clips1,clips2),num_features);
-    FF1=FF(:,1:N1);
-    FF2=FF(:,N1+1:N1+N2);
+S1=segments{1};
+features1=features(:,S1.inds);
+%labels1=isosplit5(features1);
+labels(S1.inds)=labels1;
+figure; ms_view_clusters(features1,labels1);
+figure; ms_view_templates_from_clips(clips(:,:,S1.inds),labels1)
 
-    idx1=knnsearch(FF1',FF2','K',num_neighbors);
-    clips2_1=zeros(size(clips2));
-    for ii=1:num_neighbors
-        clips2_1=clips2_1+clips1(:,:,idx1(:,ii));
-    end;
-    clips2_1=clips2_1/num_neighbors;
+%for kk=2:length(segments)
+for kk=2:3
+    S_kk=segments{kk};
+    features_kk=features(:,S_kk.inds);
+    labels_kk=labels(S_kk.inds);
+    opts.initial_labels=labels_kk;
+    new_labels_kk=isosplit5(features_kk,opts);
+    inds0=find(labels(S_kk.inds)==0);
+    labels(S_kk.inds(inds0))=labels_kk(inds0);
     
-    idx2=knnsearch(FF2',FF2','K',num_neighbors+1);
-    clips2_2=zeros(size(clips2));
-    for ii=2:num_neighbors+1
-        clips2_2=clips2_2+clips2(:,:,idx2(:,ii));
-    end;
-    clips2_2=clips2_2/num_neighbors;
-
-    segments{j}.clips_traced=clips2+clips2_1-clips2_2;
+    figure; ms_view_clusters(features_kk,labels_kk);
+    figure; ms_view_templates_from_clips(clips(:,:,S_kk.inds),labels_kk)
 end;
-
-clips_traced=zeros(size(clips,1),size(clips,2),0);
-for j=1:length(segments)
-    clips_traced=cat(3,clips_traced,segments{j}.clips_traced);
-end;
-
-FF=ms_event_features(clips,num_features);
-FF_traced=ms_event_features(clips_traced,num_features);
 
